905341427f7e99b6e9a2e80f0fdccd4bd65113f4
tranzi90 <tranzi90@gmail.com>
2020-01-21 23:32:53 +0300

    Updates

---
+++ precache-manifest.1f0ea81ff013cf7c57807f50f444e1e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c39423a49a7e6149fc3802d216e1ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1c50717e513891ea6861",
+    "url": "/monsters-rolodex/static/css/main.4bc451c5.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c50717e513891ea6861",
+    "url": "/monsters-rolodex/static/js/main.a209464c.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
